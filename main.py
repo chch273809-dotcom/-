90ccb82a8d9c59f473834a4804d9a33e37956a76
야무진현미차 <chch273809@gmail.com>
2025-11-05 09:12:05 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
 st.title('나의 첫 웹 서비스 만들기!')
-st.text_input('나마에오카이테구다사이')
-st.button('인사말 생성')
+a=st.text_input('나마에오카이테구다사이')
+if st.button('인사말 생성'):
+  st.write(a+'님 안녕하세요')
